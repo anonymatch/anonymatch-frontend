060736913057f1d2df887a9e78be9adfda7d8297
anonymatch <anonymatch.eth@gmail.com>
2024-11-17 09:09:41 +0700

    wip backend with firestore

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,12 @@
 import { areUsersSimilar } from "@/ai/actions";
 
 export async function GET() {
+  // const chatId = '
   // TODO: get user from DB FIRESTORE
+  const chat = {
+    messages: []
+  }
   return Response.json(
-    
+    chat
   );
 }
